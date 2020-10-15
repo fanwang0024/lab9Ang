a8d7717c223d0dd86dc82a721d8d4e0f61203a8a
Fan Wang <fwan0024@student.monash.edu>
2020-10-16 10:25:20 +1100

    initial commit

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from "../database.service";
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+
+  moviesDB: any[] = [];
+  section = 1;
+  title: string = "";
+  year: number = 0;
+  movieId: string = "";
+  aYear: number = 0;
+
+  constructor(private dbService: DatabaseService) { }
+
+  onGetMovies() {
+    this.dbService.getMovies().subscribe((data: any[]) => {
+      this.moviesDB = data;
+    });
+  }
+  onSaveMovie() {
+    let obj = { title: this.title, year: this.year };
+    this.dbService.createMovie(obj).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+  onDeleteMovie(item) {
+    this.dbService.deleteMovie(item._id).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+
+  onDeleteMoviesByYear(aYear){
+    this.dbService.deleteMoviesByYear(aYear).subscribe(result =>{
+      this.onGetMovies();
+    });
+  }
+
+  ngOnInit(): void {
+    this.onGetMovies();
+  }
+
+  changeSection(sectionId) {
+    this.section = sectionId;
+    this.resetValues();
+  }
+  resetValues() {
+    this.title = "";
+    this.year = 0;
+    this.movieId = "";
+  }
+
+}
